ed4ae96ca7780c9517b4aa8eff9e1acabd54dd4a
noe <noe.jaraq@gmail.com>
2023-04-18 20:36:29 -0500

    archivos base

---
+++ src/main/java/com/example/lab4_20203248/Lab420203248Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20203248;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420203248Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420203248Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_20203248/Lab420203248ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20203248;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420203248ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
